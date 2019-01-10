0d639ec33c06a3af4bc247cbef8413767d573111
Marci Clark <marcig612@gmail.com>
2019-01-10 13:27:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ describe "#speak_to_grandma" do
     it "responds with NO, NOT SINCE 1938! When she can hear you" do
       expect(speak_to_grandma("How are you?")).to eq "NO, NOT SINCE 1938!"
       elsif
-      it "responds with I LOVE YOU TOO PUMPKIN!"
+      it "responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!"
 
 
 
